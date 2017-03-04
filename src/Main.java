943303a13e5931dffe56985711412447fb976e89
olwj <al.wajgert@gmail.com>
2017-03-04 13:07:34 +0100

    add final files

---
+++ src/Main.java
@@ -0,0 +1,81 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        int a = 4;
+        int b = 2;
+
+        if ((a % b) == 0) {
+            System.out.printf("Dzieli sie");
+        } else {
+            System.out.printf("Nie dzieli sie");
+        }
+
+        b = 765;
+        a = 43;
+
+        if ((a % b) ==0) {
+            System.out.println("Dzieli sie");
+        }else {
+            System.out.println("Nie dzieli sie");
+        }
+
+        int c = 24;
+
+
+        if ((c % 2) ==0) {
+            System.out.println("Jest parzysta");
+        }else{
+            System.out.println("Jest nieparzysta");
+        }
+        int d = 255;
+
+
+        if((d % 2) ==0) {
+            System.out.println("Jest parzysta");
+        }else{
+            System.out.println("Jest nieparzysta");
+        }
+
+        int x = 5;
+        int y = 10;
+        int z = 15;
+
+        if (x>y) {
+            System.out.println(x + "jest wieksze od" + y);
+        }else{
+            System.out.println(x + " nie jest wieksze od "+ y);
+        }
+
+        if ((x*x) == y) {
+            System.out.println("kwadrat x jest rowny y");
+        }else{
+            System.out.println("kwadrat x nie jest rowny y");
+        }
+
+        if ((x+y) == z) {
+            System.out.println("suma x i y jest rowna z");
+        }else{
+            System.out.println("suma x i y nie jest rowna z");
+        }
+
+        if ((z-y) ==x) {
+            System.out.println("roznica z i y jest rowna x");
+        }else{
+            System.out.println("roznica z i y nie jest rowna x");
+        }
+
+        if ( !((3*x) == (z+1)) ) {
+            System.out.println("fałsz");
+        }else{
+            System.out.println("prawda");
+        }
+
+        if ((y)
+
+
+
+
+    }
+}
